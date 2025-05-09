b3a4377eb794cfdcbe6e0d2b0cc1a4b996bd1ea0
kerimli-veli <kerimovveli001@gmail.com>
2025-05-09 16:51:41 +0400

    Add project files.

---
+++ DirectExthange.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer");
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new("amqps://fpuoiyor:t8SZYf8YmxPfSnvxwPiWbKYJwjfmNWlW@goose.rmq2.cloudamqp.com/fpuoiyor");
+
+
+using IConnection connection = await factory.CreateConnectionAsync();
+
+using IChannel channel = await connection.CreateChannelAsync();
+
+
+
+await channel.ExchangeDeclareAsync(exchange: "direct-example-wirt-my-group", type: ExchangeType.Direct);
+
+
+string queueName = channel.QueueDeclareAsync().Result.QueueName;
+
+await channel.QueueBindAsync(
+    queue: queueName,
+    exchange: "direct-example-wirt-my-group",
+          routingKey: "direct-queue"
+         );
+
+
+
+AsyncEventingBasicConsumer consumer = new(channel);
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+
+
+
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+Console.Read();
---
+++ DirectExthange.Publiser/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Publiser");
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://fpuoiyor:t8SZYf8YmxPfSnvxwPiWbKYJwjfmNWlW@goose.rmq2.cloudamqp.com/fpuoiyor");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+
+
+
+using IChannel channel = await connection.CreateChannelAsync();
+
+
+await channel.ExchangeDeclareAsync(exchange: "direct-example-wirt-my-group", type: ExchangeType.Direct);
+
+
+while (true)
+{
+    Console.WriteLine("Mesaj: ");
+    string message = Console.ReadLine();
+    byte[]  byteMessage = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+          exchange: "direct-example-wirt-my-group",
+          routingKey: "direct-queue",
+          body:byteMessage
+         );
+}
